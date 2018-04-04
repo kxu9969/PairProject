3681a2b96ee1994b5b8060e0028ac0f0dc77a7e4
kxu9969 <kxu9969@CHHB2ZJHXV1.inst.hcpss.org>
2018-04-04 14:09:59 -0400

    stuff

--- Pair Project/src/Game.java
+++ Pair Project/src/Game.java
@@ -1,7 +1,27 @@
 import javax.swing.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Game extends JFrame{
 	Visuals vis;
 	Player p;
+	static Timer t = new Timer();
+	
+	Game(){
+		p = new Player();
+		vis = new Visuals();
+		this.add(vis);
+		this.pack();
+		this.setVisible(true);		
+		t.schedule(new TimerTask(){
+			public void run() {
+				vis.repaint();
+				//also calculate crap
+			}
+		}, 0, 10);
+	}
+	
+	
+	
 	
 }
