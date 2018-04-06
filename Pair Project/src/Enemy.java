c5930e9963eb16c06a2c6d150424c82b49016b4b
kxu9969 <kxu9969@CHHB2ZJHXV1.inst.hcpss.org>
2018-04-06 13:59:42 -0400

    visuals

--- Pair Project/src/Enemy.java
+++ Pair Project/src/Enemy.java
@@ -2,8 +2,13 @@
 public class Enemy {
 	Hitbox hitbox;
 	int[] increment = {0,0};//needs functionality
+	final int DEFAULT_START_X = 0;
+	final int DEFAULT_START_Y = 0;
+	final int WIDTH = 10;
+	final int HEIGHT = 10;
 	Enemy(){
-		
+		hitbox = new Hitbox(new Coordinate(DEFAULT_START_X,DEFAULT_START_Y),
+				new Coordinate(DEFAULT_START_X+WIDTH,DEFAULT_START_Y+HEIGHT));
 	}
 	void move(){
 		hitbox.move(increment[0], increment[1]);
