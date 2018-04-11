92fc3f49acef8025dbc087627b1954037e00101e
kxu9969 <kxu9969@CHHB2ZJHXV1.inst.hcpss.org>
2018-04-11 13:54:19 -0400

    eh

--- Pair Project/src/Game.java
+++ Pair Project/src/Game.java
@@ -18,7 +18,7 @@ public class Game extends JFrame implements KeyListener{
 	ArrayList<Bullet> toBeRemoved = new ArrayList<Bullet>();
 	ArrayList<Enemy> ded = new ArrayList<Enemy>();
 	static Timer t = new Timer();
-	final int INCREMENT_AMOUNT = 5;
+	final int INCREMENT_AMOUNT = 2;
 
 
 	Game(String playerName){
@@ -34,12 +34,6 @@ public class Game extends JFrame implements KeyListener{
 			public void run() {
 				if(!p.dead){
 					p.move();
-					if(p.counterDelay == 0){
-						playerBullets.add(new Bullet(p.hitbox.c1,new int[]{0,-p.bulletSpeed}));
-						p.counterDelay = p.counterMax;
-					}else{
-						p.counterDelay--;
-					}
 					for(Enemy e: enemies){
 						e.move();
 						//System.out.println(e.hitbox.c1.x+" "+e.hitbox.c1.y+" "+e.hitbox.c2.x+" "+e.hitbox.c2.y);
@@ -49,6 +43,12 @@ public class Game extends JFrame implements KeyListener{
 						}else{
 							e.counterDelay--;
 						}
+					}
+					if(p.counterDelay == 0){
+						playerBullets.add(new Bullet(p.hitbox.c1,new int[]{0,-p.bulletSpeed}));
+						p.counterDelay = p.counterMax;
+					}else{
+						p.counterDelay--;
 					}
 					for(Bullet b: playerBullets){
 						b.move();
